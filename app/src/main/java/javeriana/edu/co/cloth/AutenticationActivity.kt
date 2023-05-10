995a1ef6fbb1c6d30bcb01b0a64afad8a04a7e3d
Betico1928 <betoveluis@gmail.com>
2023-05-09 19:28:09 -0500

    Logo added

--- app/src/main/java/javeriana/edu/co/cloth/AutenticationActivity.kt
+++ app/src/main/java/javeriana/edu/co/cloth/AutenticationActivity.kt
@@ -3,11 +3,11 @@ package javeriana.edu.co.cloth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity()
+class AutenticationActivity : AppCompatActivity()
 {
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_autentication)
     }
 }
