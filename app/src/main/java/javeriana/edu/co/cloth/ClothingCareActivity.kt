df0b89b8278fb012baa28df62395b4139aeeed63
Betico1928 <betoveluis@gmail.com>
2023-05-30 18:48:46 -0500

    Created ClothingCare extension and intents between activities

--- app/src/main/java/javeriana/edu/co/cloth/ClothingCareActivity.kt
+++ app/src/main/java/javeriana/edu/co/cloth/ClothingCareActivity.kt
@@ -1,5 +1,6 @@
 package javeriana.edu.co.cloth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import javeriana.edu.co.cloth.databinding.ActivityClothingCareBinding
@@ -9,10 +10,96 @@ class ClothingCareActivity : AppCompatActivity()
 
     private lateinit var clothingCareBinding : ActivityClothingCareBinding
 
+    private var clothType : String? = null
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         clothingCareBinding = ActivityClothingCareBinding.inflate(layoutInflater)
         setContentView(clothingCareBinding.root)
+
+        initializeElements()
+    }
+
+    private fun initializeElements()
+    {
+        // Wool
+        clothingCareBinding.imageViewWool.setOnClickListener {
+            clothType = "wool"
+
+            val pasarAClothingCareExtension = Intent(baseContext, ClothingCareExtensionActivity::class.java)
+            pasarAClothingCareExtension.putExtra("clothType", clothType)
+            startActivity(pasarAClothingCareExtension)
+        }
+
+        clothingCareBinding.textViewWool.setOnClickListener {
+            clothType = "wool"
+
+            val pasarAClothingCareExtension = Intent(baseContext, ClothingCareExtensionActivity::class.java)
+            pasarAClothingCareExtension.putExtra("clothType", clothType)
+            startActivity(pasarAClothingCareExtension)
+        }
+
+
+
+        // Cotton
+        clothingCareBinding.imageViewCotton.setOnClickListener {
+            clothType = "cotton"
+
+            val pasarAClothingCareExtension = Intent(baseContext, ClothingCareExtensionActivity::class.java)
+            pasarAClothingCareExtension.putExtra("clothType", clothType)
+            startActivity(pasarAClothingCareExtension)
+        }
+
+        clothingCareBinding.textViewCotton.setOnClickListener {
+            clothType = "cotton"
+
+            val pasarAClothingCareExtension =
+                Intent(baseContext, ClothingCareExtensionActivity::class.java)
+            pasarAClothingCareExtension.putExtra("clothType", clothType)
+            startActivity(pasarAClothingCareExtension)
+        }
+
+
+
+        // Jeans
+        clothingCareBinding.imageViewJeans.setOnClickListener {
+            clothType = "jeans"
+
+            val pasarAClothingCareExtension =
+                Intent(baseContext, ClothingCareExtensionActivity::class.java)
+            pasarAClothingCareExtension.putExtra("clothType", clothType)
+            startActivity(pasarAClothingCareExtension)
+        }
+
+        clothingCareBinding.textViewJeans.setOnClickListener {
+            clothType = "jeans"
+
+            val pasarAClothingCareExtension =
+                Intent(baseContext, ClothingCareExtensionActivity::class.java)
+            pasarAClothingCareExtension.putExtra("clothType", clothType)
+            startActivity(pasarAClothingCareExtension)
+        }
+
+
+
+        // Poliester
+        clothingCareBinding.imageViewPoliester.setOnClickListener {
+            clothType = "poliester"
+
+            val pasarAClothingCareExtension =
+                Intent(baseContext, ClothingCareExtensionActivity::class.java)
+            pasarAClothingCareExtension.putExtra("clothType", clothType)
+            startActivity(pasarAClothingCareExtension)
+        }
+
+        clothingCareBinding.textViewPoliester.setOnClickListener {
+            clothType = "poliester"
+
+            val pasarAClothingCareExtension =
+                Intent(baseContext, ClothingCareExtensionActivity::class.java)
+            pasarAClothingCareExtension.putExtra("clothType", clothType)
+            startActivity(pasarAClothingCareExtension)
+        }
     }
 }
---
+++ app/src/main/java/javeriana/edu/co/cloth/ClothingCareExtensionActivity.kt
@@ -0,0 +1,11 @@
+package javeriana.edu.co.cloth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ClothingCareExtensionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clothing_care_extension)
+    }
+}
